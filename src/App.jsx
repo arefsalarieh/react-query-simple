69163dcf4c73636c0eed10d8ce9a1cea0d201f67
aref <arefsalarieh@gmail.com>
2024-10-30 23:56:42 +0400

    useInfiniteQuery

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Layout from './component/Layout'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import AddCard from './component/AddCard'
 import Detail from './component/Detail'
+import PeopleList from './component/PeopleList'
 
 function App() {
   const client = new QueryClient({defaultOptions: {queries: {refetchOnWindowFocus: true,  cacheTime:6000, }, mutations:{}}})
@@ -21,6 +22,10 @@ function App() {
           path:'/',
           element:<List/>
         },
+        {
+          path:'/People',
+          element:<PeopleList/>
+        },        
         {
           path:'/Detail/:id',
           element:<Detail/>
--- src/component/List.jsx
+++ src/component/List.jsx
@@ -1,44 +1,43 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import { useQuery, useQueryClient } from 'react-query'
-import AddCard from './AddCard'
-import CourseCard from './CourseCard'
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useQuery, useQueryClient } from "react-query";
+import AddCard from "./AddCard";
+import CourseCard from "./CourseCard";
 
 const List = () => {
-    
+  const getList = async () => {
+    const result = await axios.get(
+      "https://66e301e5494df9a478e3f4f6.mockapi.io/test/test"
+    );
+    return result.data;
+  };
 
-
-    const getList = async ()=>{
-        const result = await axios.get('https://66e301e5494df9a478e3f4f6.mockapi.io/test/test')
-        return result.data
-    }
-
-    const {data , status , isFetching} = useQuery('list' , getList)
-
-
-
-
+  const { data, status, isFetching } = useQuery("list", getList);
 
   return (
     <div>
+      <AddCard />
+      {status === "loading" && (
+        <h1 className="mt-10 text-center font-extrabold text-2xl">loading</h1>
+      )}
 
-            <AddCard/>
-            {status === 'loading' && <h1 className='mt-10 text-center font-extrabold text-2xl'>loading</h1>}
+      {status === "success" && (
+        <div className="mt-10 text-center ">
+          <h1 className="font-extrabold text-2xl">Courses list</h1>
+          <p className="w-fit shadow-2xl mx-auto bg-gray-200">
+            get data with useQuery
+          </p>
+        </div>
+      )}
 
-            {status === 'success' && <h1 className='mt-10 text-center font-extrabold text-2xl'>Courses list</h1>}
-
-
-            <div  className='flex flex-wrap justify-between gap-5 w-[90%] mx-auto'>
-
-                {data && data.map((item , index)=>{
-                    return(
-                        <CourseCard item={item}/>
-                    )
-                })}
-            </div>
-
+      <div className="flex flex-wrap justify-between gap-5 w-[90%] mx-auto mt-5">
+        {data &&
+          data.map((item, index) => {
+            return <CourseCard item={item} />;
+          })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
